90bf9f8f8873bf5f24251323445568d2b648e751
Ghislain Byimbo <byimbog250@gmail.com>
2025-07-31 21:58:46 +0200

    nineth commit

---
+++ src/jokeA.jsx
@@ -0,0 +1,22 @@
+export default [
+    [
+        joke = "HELOO MY NAME IS MR BEAN I LOVE MUSIC",
+        setup = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas aliquam quidem officia incidunt, odit mollitia, laborum unde facere explicabo dolorum laudantium, dignissimos soluta cupiditate nulla! Similique fugiat quibusdam excepturi non."
+    ]
+    [
+        joke = "HELOO MY NAME IS MR BEAN I LOVE MUSIC",
+        setup = "officia incidunt, odit mollitia, laborum unde facere explicabo dolorum laudantium, dignissimos soluta cupiditate nulla! Similique fugiat quibusdam excepturi non."
+    ]
+    [
+        joke = "HELOO MY NAME IS MR BEAN I LOVE MUSIC",
+        setup = "Lorem ipsum dolor sit amet consectetur adipisicing elit.xplicabo dolorum laudantium, dignissimos soluta cupiditate nulla! Similique fugiat quibusdam excepturi non."
+    ]
+    [
+        joke = "HELOO MY NAME IS MR BEAN I LOVE MUSIC",
+        setup = "odit mollitia, laborum unde facere explicabo dolorum laudantium, dignissimos soluta cupiditate nulla! Similique fugiat quibusdam excepturi non."
+    ]
+    [
+        joke = "HELOO MY NAME IS MR BEAN I LOVE MUSIC",
+        setup = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas aliquam quidem officia incidunt, odit mollitia, laborum unde facere explicabo dolorum laudantium, dignissimos soluta cupiditate nulla! Similique fugiat quibusdam excepturi non."
+    ]
+]
